030ca3592c1ee48336334894791b6d27d60b1527
Vilmos Czeroczky <vxc653@cs.bham.ac.uk>
2017-03-29 03:07:03 +0100

    added data members

--- block.js
+++ block.js
@@ -1,22 +1,7 @@
 class Block {
-	constructor(ctx) {
-		this.ctx = ctx;
-	}
-
-	getArea() {
-		return this.height * this.width;
-	}
-
-	drawBlock() {
-		var c = document.createElement("canvas");
-		c.width = c.height = 10;
-		var _ctx = c.getContext("2d");
-
-		_ctx.fillStyle = "grey";
-
-		_ctx.fillRect(0, 0, 100, 100);
-		var img = new Image();
-		img.src = c.toDataURL();
-		this.ctx.drawImage(img, 1, 1)
+	constructor() {
+		this.expanded = false;
+		this.isMine = false;
+		this.n = 0;
 	}
 }
