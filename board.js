49917ed6d008dde3df537399700474655245af7e
Vilmos Czeroczky <vxc653@cs.bham.ac.uk>
2017-03-29 15:49:53 +0100

    generate mines and neighbours

--- board.js
+++ board.js
@@ -15,6 +15,13 @@ class Board {
 				this.drawBlock(i,j);
 			}
 		}
+
+		this.generateMines();
+		for (var i = 0; i < width; i++) {
+			for (var j = 0; j < height; j++) {
+				this.drawBlock(i,j);
+			}
+		}
 	}
 
 	drawBoard() {
@@ -52,5 +59,41 @@ class Board {
 		var img = new Image();
 		img.src = blockCanvas.toDataURL();
 		this.ctx.drawImage(img, (x * 20) + (x + 1), (y * 20) + (y + 1))
+	}
+
+	generateMines() {
+		var minesLeft = this.numMines;
+		while (minesLeft != 0) {
+			var xPos = Math.trunc(Math.random() * this.width); // Generate a number from 0 to the upper bound (exclusive)
+			var yPos = Math.trunc(Math.random() * this.height);
+			// Check if the block is not a mine already
+			if (!this.blocks[xPos][yPos].isMine) {
+				// Set the block to be a mine
+				this.blocks[xPos][yPos].isMine = true;
+				this.drawBlock(xPos, yPos);
+				// Increase the number of the neighbours of the mine
+				var neighbours = this.getNeighboursOfBlock(xPos, yPos);
+				for (var i = 0; i < neighbours.length; i++) {
+					neighbours[i].incrementN();
+				}
+				minesLeft--;
+			}
+		}
+	}
+
+	getNeighboursOfBlock(x, y) {
+		var neighbours = [];
+		for (var i = (x - 1); i <= (x + 1); i++) {
+			for (var j = (y - 1); j <= (y + 1); j++) {
+				if (!(i == x && j == y) && !this.isOutOfRange(i, j)) {
+					neighbours.push(this.blocks[i][j]);
+				}
+			}
+		}
+		return neighbours;
+	}
+
+	isOutOfRange(x, y) {
+		return ((Math.min(x, y) < 0) || (x > (this.width - 1)) || (y > (this.height - 1)));
 	}
 }
