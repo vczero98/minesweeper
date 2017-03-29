d163420cde16685dbff2049cdcd162fc7b69fc70
Vilmos Czeroczky <vxc653@cs.bham.ac.uk>
2017-03-29 20:05:07 +0100

    moved js files to js folder

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	board = new Board(16, 30, 99);
-	//board = new Board(10, 10, 1);
+	//board = new Board(10, 10, 5);
 	function printParagraph(text) {
 		document.write("<p>" + text + "</p>");
 	}
