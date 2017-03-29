d46a2f7188886c2a94b83153c8f74586e2bde817
Vilmos Czeroczky <vxc653@cs.bham.ac.uk>
2017-03-29 15:50:26 +0100

    run after the page loads

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var b = new Block(4, 5);
-printParagraph("Area: " + b.getArea());
-//document.body.style.cursor = "url('axe.png'), auto";
-board = new Board(10, 10, 10);
-function printParagraph(text) {
-	document.write("<p>" + text + "</p>");
+window.onload = function() {
+	board = new Board(16, 30, 99);
+	//board = new Board(10, 10, 1);
+	function printParagraph(text) {
+		document.write("<p>" + text + "</p>");
+	}
 }
